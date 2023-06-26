9a07be0f222acef63dc725e4b71aeb93c4d5cd4b
nruxshod07 <nezametdinovruxsod@gmail.com>
2023-06-26 18:22:08 +0500

    New

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-let arr = [{aaa:{price:20}},{aaa:{price:35}},{aaa:{price:44}}]
-let total = arr[0].aaa.price + arr[1].aaa.price + arr[2].aaa.price
+let arr = [[{aaa:{price:20}}],[{aaa:{price:35}}],[{aaa:{price:44}}]]
+let total = arr[0][0].aaa.price + arr[1][0].aaa.price + arr[2][0].aaa.price
 console.log(
     total
 );    
